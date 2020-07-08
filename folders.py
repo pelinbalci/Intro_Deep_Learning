c0c36e81de60a67d4ea19f3441edaac8acf0998d
Pelin Balci <balci.pelin@gmail.com>
2020-07-08 10:18:59 +0300

    folders names are fixed

--- folders.py
+++ folders.py
@@ -1,6 +1,5 @@
 import os
 
 folder = {'main_folder': os.path.dirname(os.path.abspath(__file__)),
-          'CNN_images': 'CNN/notes/images',
           'Intro_NN_inputs': '/Intro_NN/inputs'
           }
